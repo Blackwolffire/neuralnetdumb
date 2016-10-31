9f87614765835f318ff830770b36b13c5e2897e7
besson_a <nicolas.bessonnet@epita.fr>
2016-10-31 04:14:45 +0100

    ...

--- segment.c
+++ segment.c
@@ -1,3 +1,4 @@
+r
 #include <stdio.h>
 #include <stdlib.h>
 #include <SDL.h>
@@ -55,43 +56,69 @@ void putpixel(SDL_Surface *surface, unsigned x, unsigned y, Uint32 pixel) {
 //largeur
 //besoin d un teste pour corriger la hauteur d une ligne a chaque ligne et la
 //changer avectline
-//besoin de continuer a tester en verifiant qu l on ai pas fini le bloc
+//besoin de continuer a tester apres que l on ai pas fini le bloc
 void detectblock(SDL_Surface* img){
-  int x,y,e,tline,y1,y2;
+  int x,y,e,tline,y1,y2,x2;
   int rgb;
+  int a = 0;
   int b = 0; 
   int c = 0;
   int d = 0;
-  for(y = 0; y < img -> h;y++){  //seulement a faire pour la premier ligne apres c est easy
-    e = 0;
-    for(x = 0; x < img -> w && e; x++){
-      if( isblack(img,x,y) && d){
+  int e = 0;
+  for(y = 0; y < img -> h;y++){ 
+    for(x = 0; x < img -> w && e; x++){//traitement premiere ligne du bloc
+      if( isblack(img,x,y) && d){//repeter au debut de chaque bloc pour fixer une premiere valeur de tline DEBUT DU BLOC
         x1=x;
+        //x2 = x car dernier pixel noir du bloc quand on debute le bloc  x2 est le prenier
         y1=y;
-        for(;y<img -> h && b  ;y++){  // b est un booleen true tant que ligne pas sans noir
+        for(;y<img -> h && b  ;y++){  // b est un booleen true tant que ligne contient au moind un p noir
           b = 1;
           c = 0;
           for(x=0;x<img -> w && c ;x++){ 
             if(isblack(img,x,y)){
+              //if(x2<x)
+              //  x2 =x;
               b = 0;
               c = 1;  
             }
           }
         }
-        tline = y1 - y;
-        y2 = y
+        tline = y - y1;
+        y2 = y;
         d = 1;
-      }
-      
+      }                
     }
-    if(y-y2 == tline && endblock == true)//fin du bloc nous pouvons maintenant
-// notre struct de blocs chaine et reininitialiser la plupart sauf y et en
-// sauvegardant y1 ypour sa hauteur,,,,,,,,,
+    //traitement jusqu a debut de ligne suivante
+    for(;y<img->y ;y++){
+      for(;x<img->x && a;x++){
+        if( y-y2 < tline && isblack(img,x,y) ){//set y2 a la valeur y qund on commence la prochaine ligne
+           a =1
+           y2 = y
+        }
+        isblack(img,x,y)
+      }
+      if(y-y2 >= tline && isblack(img,x,y) ){
+           // endbloc = 0 or true if ligne de y2 contient un pixel noi
+           //fin du bloc nous pouvons maintenant
+           // notre struct de blocs chaine et reininitialiser la plupart sauf y et en
+           // creer un nouveau block sauvegarder l ancien,,,,,,,,,
+      a = 1;
+      }
+   }
+   //traitement de la ligne
+     //traitement de x1 si x<x1 et x noir x1=x seulement si meme bloc donc on passe par un x3
+     if(y-y2<tline && endline){//se repete tant que y-y2<tline || rencontre une  ligne sanspixel noir pour si  taille de ligne  != tline alors debut d un nouveau bloc et enregistrement du precedan
+
+
+     //a la fin du traitement de la ligne mettre y2 a 0
+     }
+     //oublie pas de mettre x2 a jour
   }
 }
 
-int isblack(SDL_Surface* img,int x, y){
-  Uint32 rgb = getpixel(img,x,y);
+int isblack(SDL_Surface* img,int x,int y){
+  Uint32 rgb = getpixel(img,x,y)
+  return (!(rgb & 0x00ffffff));
   // si r=g=b=255 return 0 else return 1
   
 }
