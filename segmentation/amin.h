78d98385950fb6ee6e30d8c54f13d07770d3c182
Damien Molliere <damien.molliere@epita.fr>
2016-12-04 12:47:44 +0100

    modif of gabi

--- segmentation/amin.h
+++ segmentation/amin.h
@@ -1,5 +1,17 @@
 #ifndef AMIN_H
 #define AMIN_H
+# include <SDL/SDL.h>
+# include <SDL/SDL_image.h>
+# include "../neuralnet/list.h"
+
+struct coord
+{
+  int xmin;
+  int xmax;
+  int ymin;
+  int ymax;
+};
+
 
 void init_sdl(void);
 SDL_Surface *screen();
