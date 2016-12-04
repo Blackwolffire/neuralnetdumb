3589196161f913c62510fe2312f31e70f6942212
Amin Sdouga <amin.sdouga@epita.fr>
2016-12-04 15:08:43 +0100

    waiting

--- training/main.c
+++ training/main.c
@@ -2,19 +2,27 @@
  * sdouga_a
  */
 
-#include <sdtlib.h>
+#include <stdlib.h>
 #include <SDL/SDL.h>
-#include <SDL/SDL_image.hi>
+#include <SDL/SDL_image.h>
 
-#include "amin.h"
+//#include "../segmentation/amin.h"
 
-int main(int argc, char **argv)
+int main(int argc, char	**argv)
 {
-	init_sdl();
+	size_t w = (256 + 76) / 2;
+	NeuralNet *net = NULL;
 
+	if(argv[1][0] == 'y')
+		net = loadNeural("net.sav");
+	else
+		net = createNeural(256, 76, 1, &w, SIGMOID);
 
+	for(size_t i = 3; i < argc; ++i){
+		///////////////////
+		training(net, list, outputs, (flint)0.01, list->len, strtoul(argv[2]));
+	}
 
-	SDL_Quit();
 	return EXIT_SUCCESS;
 }
 
--- training/training.c
+++ training/training.c
@@ -2,5 +2,24 @@
  *	sdouga_a
  */
 
+void training(NeuralNet *net,List *letters,flint *outputs, flint eta,
+							size_t nbTrain,	size_t len, char *filename)
+{
+	flint *inputs;
+	struct matrice *mat;
 
+	assert(net && letters && outputs && filename);
+
+	inputs = malloc(sizeof(flint) * 16 * list->len);
+
+	for(size_t j = 0; j < nbTrain; ++j){
+		mat = getDataList(list, j);
+		printf("%d\n", mat->width * mat->height);
+		for(size_t k = 0; k < 256; ++k)
+			inputs[j * 256 + k].fl = mat[k];
+	}
+	trainingNeural(net, inputs, outputs, nbTrain, eta, len, filename);
+
+	free(inputs);
+}
 
